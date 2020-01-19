b73cda3f47c6007f24d63098d59c22c8b51e8073
Denis <denisov.denis.andreevich@gmail.com>
2020-01-19 17:59:45 +0300

    webApp

--- src/main/java/Application/View.java
+++ src/main/java/Application/View.java
@@ -6,12 +6,10 @@ import Entity.FinalNode;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.w3c.dom.Document;
 
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.Objects;
+
 
 @Controller
 public class View {
@@ -22,18 +20,27 @@ public class View {
 
 
 @GetMapping("/View")
-public String start(@RequestParam(name="name", required=false, defaultValue="World") String ipNode, Model model) {
-    NodeBuildDAO nodeBuildDAO =new NodeBuildDAO();
+public String start(Model model) {
+    NodeBuildDAO nodeBuildDAO = new NodeBuildDAO();
+    /*ArrayList<Object>  idCol = new ArrayList<>();
+    ArrayList<Object>  dateCol = new ArrayList<>();
+    ArrayList<Object>  ipNodeCol= new ArrayList<>();*/
 
-   for (int i=0;i!=nodeBuildDAO.findByIpNode().size();i++) {
+    ArrayList<Object> nodes =new ArrayList<>();
+
+    for (int i = 0; i != nodeBuildDAO.findByIpNode().size(); i++) {
+        nodes.add(nodeBuildDAO.findByIpNode().get(i).getId());
+        nodes.add(nodeBuildDAO.findByIpNode().get(i).getDate());
+        nodes.add(nodeBuildDAO.findByIpNode().get(i).getIpNode());
+
+        model.addAttribute("nodes", nodes);
 
 
-       model.addAttribute("name", ipNode=nodeBuildDAO.findByIpNode().get(i).getIpNode());
 
-       return "main";
-   }
+
+
+    }
     return "main";
 }
-
 
 }
